d5a98c4e110bb8c5e9d82e872a1ae60e6c2d776a
macswell <macs.well@gmail.com>
2023-02-15 12:24:48 -0800

    Changes to installer script and config

--- LinuxKiosk.sh
+++ LinuxKiosk.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Set the working directory to the directory from which the script is run
+cd "$(dirname "$0")"
+
 # Read the input file containing the list of applications and resources to download
-input_file="app_list.xml"
+input_file="LinuxKioskConfig.xml"
 
 # Set the log file name and location
 log_file="install.log"
+
+# Set the temporary download directory
+temp_dir="Linux Kiosk Temp"
 
 # Install required packages using apt
 install_packages() {
@@ -22,7 +28,7 @@ install_packages() {
 download_file() {
     local filename="$1"
     local url="$2"
-    if ! wget -O "$filename" "$url" >> "$log_file" 2>&1; then
+    if ! wget -P "$temp_dir" -q --show-progress "$url" >> "$log_file" 2>&1; then
         echo "Error downloading file $filename from $url. Aborting script." >&2
         remove_downloaded_files
         exit 1
@@ -43,14 +49,9 @@ run_script() {
 
 # Remove downloaded files
 remove_downloaded_files() {
-    while IFS= read -r line; do
-        if [[ "$line" == *"<file name=\""* ]]; then
-            filename=$(echo "$line" | sed -e 's/<file name=\"\(.*\)\" url=\".*\"\/>/\1/')
-            if [ -e "$filename" ]; then
-                rm "$filename"
-            fi
-        fi
-    done < "$input_file"
+    if [ -d "$temp_dir" ]; then
+        rm -r "$temp_dir"
+    fi
 }
 
 # Remove installed packages
@@ -67,9 +68,11 @@ remove_packages() {
     fi
 }
 
-# Parse the input file and install packages, download files and run scripts as defined
+# Parse the input file, install packages, download files and run scripts as defined
 parse_input_file() {
     local packages=()
+    local files=()
+    local scripts=()
     while IFS= read -r line; do
         case "$line" in
             *"<package>"*)
@@ -79,18 +82,21 @@ parse_input_file() {
             *"<file name=\""*)
                 filename=$(echo "$line" | sed -e 's/<file name=\"\(.*\)\" url=\"\(.*\)\"\/>/\1/')
                 url=$(echo "$line" | sed -e 's/<file name=\"\(.*\)\" url=\"\(.*\)\"\/>/\2/')
+                files+=("$filename")
                 download_file "$filename" "$url"
                 ;;
             *"<script>"*)
                 script=$(echo "$line" | sed -e 's/<script>\(.*\)<\/script>/\1/')
-                run_script "$script"
+                scripts+=("$script")
                 ;;
             *)
                 continue
                 ;;
         esac
     done < "$input_file"
-    install_packages "${packages[@]}"
-}
 
+    # Install packages
+    if [ ${#packages[@]} -gt 0 ]; then
+        install_packages "${packages[@]}"
+    fi
 parse_input_file
