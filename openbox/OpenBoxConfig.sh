5e80badd21da8b93eafdd92c89306738c641fd74
macswell <macs.well@gmail.com>
2023-02-15 12:27:45 -0800

    added tint2 to autostart

--- openbox/OpenBoxConfig.sh
+++ openbox/OpenBoxConfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the location of the Openbox rc.xml configuration file
-rc_file="$HOME/.config/openbox/rc.xml"
+rc_file="/etc/xdg/openbox/rc.xml"
 
 # Use awk to identify sections of code with the ShowMenu tag and comment them out
 awk '/<ShowMenu>/,/<\/ShowMenu>/ { print "<!--" $0 " -->"; next } 1' "$rc_file" > "$rc_file.temp" && mv "$rc_file.temp" "$rc_file"
@@ -18,4 +18,7 @@ new_keybind="<keybind key=\"$keybinding\" name=\"Open_Mate_Term\">
 </keybind>"
 
 # Insert the new keybinding element into the rc.xml file
-sed -i "/<\/keyboard>/i $new_keybind" ~/.config/openbox/rc.xml
+sed -i "/<\/keyboard>/i $new_keybind" /etc/xdg/openbox/rc.xml
+
+# Add Tint2 to openbox autostart
+sed -i '/tint2 &/!s/^/tint2 \&\n/' ~/.config/openbox/autostart
